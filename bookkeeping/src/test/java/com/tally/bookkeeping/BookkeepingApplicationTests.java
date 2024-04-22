aa680385738c7cc75876653988caf503f2fb1672
samirs <samirs@safexpay.com>
2024-04-22 12:08:44 +0530

    first commit

---
+++ bookkeeping/src/test/java/com/tally/bookkeeping/BookkeepingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tally.bookkeeping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookkeepingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
